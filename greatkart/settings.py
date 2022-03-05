46adc412c5ffbac63aee01581e5066ce62937d87
dAnkc0der <utkarshverma007@outlook.com>
2022-03-06 02:06:54 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -120,6 +120,7 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -133,7 +134,23 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dummyacc6975@gmail.com'
+EMAIL_HOST_PASSWORD = 'dummyacc123'
+EMAIL_USE_TLS = True
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
